363f7b049329374cb715073226bb312d9d9ca750
Suissa <jnascimento@gmail.com>
2016-12-18 01:03:28 -0200

    No suissa ja redireciona pra pagar e pah

--- suissa/routes/index.js
+++ suissa/routes/index.js
@@ -8,6 +8,7 @@ paypal.configure({
   client_id: 'Aa9rd9vRzn2zCkigJjNDq0wmybvWK1FLtIpP2zlCpbSTo5tGMVWMxFObZ2sMgc8A5Gwg_d-RIQwDaUbt',
   client_secret: 'EBW7A9RA4JSRjjXK1Mlf7yz-cAndCvG7aQf4v-6TMDORo5Q5SV8OGOzvugLcZW3nwnFFwQ2I_8P5qQq6'});
 
+
 // Build PayPal payment request
 var payReq = JSON.stringify({
   intent:'sale',
@@ -20,7 +21,7 @@ var payReq = JSON.stringify({
   },
   transactions:[{
     amount:{
-      total:'666.0',
+      total:'13',
       currency:'BRL'
     },
     description:'Pagando o estacionamento do XOPISS. \nValor: R$ 666'
@@ -30,70 +31,93 @@ var payReq = JSON.stringify({
 /* GET home page. */
 router.get('/', (req, res, next) => {
   
-  var create_web_profile_json = {
-      "name": "Webschool",
-      "presentation": {
-          "brand_name": "Best Brand",
-          "logo_image": "https://www.paypalobjects.com/webstatic/mktg/logo/AM_SbyPP_mc_vs_dc_ae.jpg",
-          "locale_code": "US"
-      },
-      "input_fields": {
-          "allow_note": true,
-          "no_shipping": 1,
-          "address_override": 1
-      },
-      "flow_config": {
-          "landing_page_type": "billing",
-          "bank_txn_pending_url": "http://www.yeowza.com"
-      }
+
+
+  var card_data = {
+    "type": "visa",
+    "number": "4417119669820331",
+    "expire_month": "11",
+    "expire_year": "2018",
+    "cvv2": "123",
+    "first_name": "Joe",
+    "last_name": "Shopper"
   };
 
-  var create_payment_json = {
-      "intent": "sale",
-      "payer": {
-          "payment_method": "paypal"
-      },
-      "redirect_urls": {
-          "return_url": "http://return.url",
-          "cancel_url": "http://cancel.url"
-      },
-      "transactions": [{
-          "item_list": {
-              "items": [{
-                  "name": "item",
-                  "sku": "item",
-                  "price": "1.00",
-                  "currency": "USD",
-                  "quantity": 1
-              }]
-          },
-          "amount": {
-              "currency": "USD",
-              "total": "1.00"
-          },
-          "description": "This is the payment description."
-      }]
-  };
+  paypal.creditCard.create(card_data, function(error, credit_card){
+    if (error) {
+      console.log(error);
+      throw error;
+    } else {
+      console.log("Create Credit-Card Response");
+      console.log(credit_card);
+    }
+  })
 
-  paypal.webProfile.create(create_web_profile_json, function (error, web_profile) {
-      if (error) {
-          throw error;
-      } else {
+  // var create_web_profile_json = {
+  //     "name": "Webschool",
+  //     "presentation": {
+  //         "brand_name": "Suissao",
+  //         "logo_image": "http://webschool.io/bemean/assets/images/logo-webschool.png",
+  //         "locale_code": "BR"
+  //     },
+  //     "input_fields": {
+  //         "allow_note": true,
+  //         "no_shipping": 1,
+  //         "address_override": 1
+  //     },
+  //     "flow_config": {
+  //         "landing_page_type": "billing",
+  //         "bank_txn_pending_url": "http://www.yeowza.com"
+  //     }
+  // };
 
-          //Set the id of the created payment experience in payment json
-          var experience_profile_id = web_profile.id;
-          create_payment_json.experience_profile_id = experience_profile_id;
+  // var create_payment_json = {
+  //     "intent": "sale",
+  //     "payer": {
+  //         "payment_method": "paypal"
+  //     },
+  //     "redirect_urls": {
+  //         "return_url": "http://localhost:3000/process",
+  //         "cancel_url": "http://localhost:3000/cancel"
+  //     },
+  //     "transactions": [{
+  //         "item_list": {
+  //             "items": [{
+  //                 "name": "Estacionamento",
+  //                 "sku": "item",
+  //                 "price": "1.00",
+  //                 "currency": "BRL",
+  //                 "quantity": 1
+  //             }]
+  //         },
+  //         "amount": {
+  //             "currency": "BRL",
+  //             "total": "1.00"
+  //         },
+  //         "description": "SUCK MY DICKOOOOOO!!"
+  //     }]
+  // };
 
-          paypal.payment.create(create_payment_json, function (error, payment) {
+  // paypal.webProfile.create(create_web_profile_json, function (error, web_profile) {
+  //     if (error) {
+  //         throw error;
+  //     } else {
+
+  //         //Set the id of the created payment experience in payment json
+  //         var experience_profile_id = web_profile.id;
+  //         create_payment_json.experience_profile_id = experience_profile_id;
+
+          paypal.payment.create(payReq, function (error, payment) {
               if (error) {
                   throw error;
               } else {
                   console.log("Create Payment Response");
                   console.log(payment);
+                  res.redirect(payment.links[1]['href'])
               }
           });
-      }
-  });
+  //     }// });
+
 })
 
 router.get('/process', (req, res, next) => {
